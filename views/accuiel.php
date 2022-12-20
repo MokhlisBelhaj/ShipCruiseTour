37b75d17d38ad336a29b197f1dadf35be51acb53
MokhlisBelhaj <belhajmokhlis0@gmail.com>
2022-12-20 17:39:16 +0100

    home

---
+++ views/accuiel.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600&family=Titillium+Web&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navb navbar1">
+    <a class="navbar-brand" href="index.php">
+      <img src="../image/logo.png" alt="logo" width="100px"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+        <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Cruis</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link " href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- <div class="test">
+    <img class="img" src="../image/back1.jpg" alt=""> -->
+    <div class="description">
+      <div class="para" >
+        <p>ShipCruiseTour is an imaginative cruise company that offers a wide range of cruise options for travelers. From luxurious ocean liners to small expedition ships, we have a cruise for every budget and preference. Our experienced team is dedicated to providing top-notch service and creating unforgettable vacation experiences for our customers. Contact us today to start planning your dream cruise!</p>
+      </div>
+    </div>
+    <div class="card-deck">
+      <div class="card">
+        <img src="../image/card1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      <div class="card">
+        <img src="../image/card2.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+      <div class="card">
+        <img src="../image/card3.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container-fluid bg-light py-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="row py-0">
+                <div class="col-sm-1">
+                  <i class="fab fa-facebook fa-lg text-primary"></i>
+                </div>
+                <div class="col-sm-1">
+                  <i class="fab fa-twitter fa-lg text-primary"></i>
+                </div>
+                <div class="col-sm-1">
+                  <i class="fab fa-google fa-lg text-primary"></i>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <p class="text-secondary text-right">Copyright &copy; 2021, ShipCruiseTour</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+  </html>
+  
+  
